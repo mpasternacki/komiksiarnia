3884251992a964caf46f9b542969b454f65e79b5
Maciej Pasternacki <maciej@pasternacki.net>
2010-12-12 19:59:25 +0100

    Admin media.

--- komiksiarnia/settings.py
+++ komiksiarnia/settings.py
@@ -45,7 +45,7 @@ MEDIA_URL = '/_static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/_static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '*_rzzvqbpm0-c@%-&3u7myl!_182ik1dudi_8o(jcvr*npn*_4'
